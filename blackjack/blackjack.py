82cd1edbab33a1a0e6dda39b58937fccc7882e82
Joshua Hecker <jhecker2001@gmail.com>
2023-10-19 09:49:23 -0500

    Imported os module, asked if user would like to play Blackjack, started game loop, printed ASCII logo.

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,7 +1,12 @@
 import art
 import random
+import os
 
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
+playing = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+
+while playing == 'y':
+    print(art.logo)
